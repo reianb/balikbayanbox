b7e1fcc7d82b239325d5bb1158571a39fab9984f
Reian Beltran <rmb20j@fsu.edu>
2022-10-26 13:22:46 -0400

    box rotates in relation to user input with mouse

--- sketch.js
+++ sketch.js
@@ -1,12 +1,26 @@
+let degreeX = 50;
+let degreeY = 30;
+let degreeZ = 1;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  describe('a box that is rotatable by interacting with a mouse');
 }
 
 function draw() {
   background(125);
-  rotateX(mouseX);
-  rotateY(mouseY);
-  frameRate(10);
-  fill(185,156,107);
-  box(500);
+  
+  rotateX(degreeX)
+  rotateY(degreeY)
+  rotateZ(degreeZ)
+  if (mouseIsPressed){
+    degreeX = -mouseY;
+    degreeY = mouseX;
+  }
+    fill(185,156,107);
+    box(400);
+}
+
+function changeBg(mouseX,mouseY){
+  //let r = map(mouseX)
 }
