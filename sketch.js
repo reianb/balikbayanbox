816cf4f6a988344dc0b3e8e2019ca5bfb9e1de5e
Reian Beltran <rmb20j@fsu.edu>
2022-10-30 16:29:12 -0400

    added assets folder and audio file

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
 
 function draw() {
   background(125);
-  if(frameCount%80==0){
+  if(frameCount%100==0){
     isRed = !isRed;
   }
   if(isRed){
@@ -19,6 +19,7 @@ function draw() {
   else{
     background("blue");
   }
+  
   rotateX(degreeX)
   rotateY(degreeY)
   rotateZ(degreeZ)
