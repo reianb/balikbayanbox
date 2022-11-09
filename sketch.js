c9789d06fad989604c21c3e973449d3920080625
Reian Beltran <rmb20j@fsu.edu>
2022-11-08 21:21:28 -0500

    audio x and y values

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function preload()
 {
   soundFormats('mp3');
   img = loadImage('assets/cardboard.jpeg');
-  for(let i = 0; i<=6;i++){
+  for(let i = 0; i<=12;i++){
     sound[i] = loadSound("assets/audio"+i+".mp3");
   }
   for(let i=0; i<=2; i++){
@@ -36,28 +36,34 @@ function draw() {
     plane(windowWidth, windowHeight);
   }
   
-  if(sound[0].isPlaying()==false&&sound[6].isPlaying()==false&&mouseX>=435&&mouseX<=785&&mouseY>=195&&mouseY<=515){
-    sound[0].play();
+  if(sound[6].isPlaying()==false&&mouseX>=435&&mouseX<=785&&mouseY>=195&&mouseY<=515){
     sound[6].play();
     }
     
-  if(sound[1].isPlaying()==false&&mouseX>=786&&mouseX<=1000&&mouseY>=195&&mouseY<=515){
-      sound[1].play();
+  if(sound[0].isPlaying()==false&&sound[7].isPlaying()==false&&mouseX>=786&&mouseX<=1000&&mouseY>=195&&mouseY<=515){
+      sound[0].play();
+      sound[7].play();
 
     } 
-  if(sound[2].isPlaying()==false&&mouseX>=1000&&mouseY>1000){
+  if(sound[2].isPlaying()==false&&sound[9].isPlaying()==false&&mouseX>=1000&&mouseY>1000){
     sound[2].play();
+    sound[9].play();
   }
   if(sound[3].isPlaying()==false&&sound[6].isPlaying()==false&&mouseX>=435&&mouseX<=785&&mouseY>=516&&mouseY<=820){
     sound[3].play();
     sound[6].play();
   }
-  if(sound[4].isPlaying()==false&&mouseX>=785&&mouseX<=1000&&mouseY>=516&&mouseY<=820){
+  if(sound[4].isPlaying()==false&&sound[8].isPlaying()==false&&mouseX>=785&&mouseX<=1000&&mouseY>=516&&mouseY<=820){
     sound[4].play();
+    sound[8].play();
   }
-  if(sound[5].isPlaying()==false&&mouseX<800&&mouseY<1000){
+  if(sound[5].isPlaying()==false&&sound[11].isPlaying()==false&&mouseX<800&&mouseY<1000){
     sound[5].play();
+    sound[11].play();
   }
+  if(sound[10].isPlaying()==false&&mouseX>=435&&mouseX<=785&&mouseY>=195&&mouseY<=515){
+    sound[10].play();
+    }
   rotateX(degreeX)
   rotateY(degreeY)
   rotateZ(degreeZ)
